15243cb1ec685381ee3005252af7cc1b972e055e
321k <erikjohansson01@gmail.com>
2016-01-10 21:16:31 +0000

    added mysql

---
+++ mysql_connection.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'transferwise',
+  password : 'johansson',
+  database : 'gt'
+});
+
+connection.connect();
+
+connection.query('SELECT * from transferwise', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+connection.end();
